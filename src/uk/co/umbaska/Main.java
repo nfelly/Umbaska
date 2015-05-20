567eb24de87d29426125d798a40c9ded3bd606b8
nfelly <nfell2009@outlook.com>
2015-05-20 12:17:06 +0100

    We don't use world

--- src/uk/co/umbaska/Main.java
+++ src/uk/co/umbaska/Main.java
@@ -18,7 +18,6 @@ import com.palmergames.bukkit.towny.object.Town;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
